291fb23200a1e130dfd5c0f1f6df6e425ebc7f60
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2025-06-01 22:47:12 +0530

    Add files via upload

---
+++ undestandingvar.py
@@ -0,0 +1,32 @@
+t1 = "Amit"
+print(t1)                              # Amit
+
+t2 = "Simran"
+t3 = "Vidhya"
+
+print("Hello",t1,t2,t3)                # Hello Amit Simran Vidhya
+
+i = "Sudarshan"
+print(i)                               # Sudarshan
+
+print("Hello",i)                       # Hello Sudarshan
+
+t1 = "Amit Varma"
+print(t1)                              # Amit Varma
+
+age = 30
+print("The number you entered was",age)  # 30
+age = 40
+print("The number you entered was",age)
+print(age)                               # 40   variable gets updated to latest value
+
+age=age+1
+print(age)
+
+a=1
+a=a+1
+print(a)   #2
+
+a=a+1;print(a)      #3
+# executing commands simultaneously
+a=1;print(a);a=a+1;print(a);a=a+1;print(a);a=a+1;print(a)   # 1 2 3 4
