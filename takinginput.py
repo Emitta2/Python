fb70293ccde8e491abef63b14034b61f4eb23d5a
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2025-06-02 10:51:16 +0530

    Add files via upload

---
+++ takinginput.py
@@ -0,0 +1,6 @@
+print("What is your name")  # What is your name
+
+n=input("What is your name") # takes input from user
+print(n)                     # rose
+print("Hello",n)              # Hello rose
+print("Hey",n,"how are you?")  # Hey rose how are you?
