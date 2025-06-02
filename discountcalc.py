ab43c80d122a62d5e441205f1e5bf692ed61a8d7
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2025-06-02 10:52:11 +0530

    Add files via upload

---
+++ discountcalc.py
@@ -0,0 +1,7 @@
+print("To compute final price after discount")
+c=input("Enter the cost of the item:")  # 200
+print(c)                                # 200 is a string
+d=int(c)                        # convert string to integer for calculation
+answer=0.9*d                    # 10% discount
+print(answer)                   # 180.0
+print("The cost after discount is",answer)  # The cost after discount is 180.0
