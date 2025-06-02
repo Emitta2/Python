e4b6179cc4723a8e96a6f5abb3718bd18839517f
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2025-06-02 10:53:31 +0530

    Add files via upload

---
+++ understandingif.py
@@ -0,0 +1,11 @@
+print("Hi How are you")
+c=input("Enter your choice:")
+d=int(c)
+if(d==1):
+    print("you entered one")
+    print("so,you are doing well")
+if(d==2):
+    print("you entered two")
+if(d==3):
+    print("Ahhh... I am tired")       
+# if you enter 4 then it doesn't print anything
