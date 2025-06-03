89b337cbb44cc8bcc175772c16574573ceb1b29c
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2025-06-03 16:24:01 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,16 @@
+# intro to loops
+for i in range(5):  # i takes values from 0 to 4
+    print("Hi,how's everything?")
+"""
+Hi,how's everything?
+Hi,how's everything?
+Hi,how's everything?  (prints 5 times)
+Hi,how's everything?
+Hi,how's everything?
+"""
+
+for i in range(5):
+    print(i)        # 0 1 2 3 4
+
+for i in range(11):  # multiples of 2 ,range(11) gives 0 to 10 to include 20
+    print(i*2)  # 0 2 4 6 8 10 12 14 16 18 20
