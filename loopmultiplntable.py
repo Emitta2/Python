70115a3a7add9688c9f965fafc0fce556040f381
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2025-06-03 16:26:06 +0530

    Add files via upload

---
+++ loopmultiplntable.py
@@ -0,0 +1,31 @@
+t=int(input("what table would you like to display?"))
+for i in range(11):
+    print(t,"X",i,"=",t*i)
+"""
+2 X 0 = 0
+2 X 1 = 2
+2 X 2 = 4
+2 X 3 = 6
+2 X 4 = 8
+2 X 5 = 10
+2 X 6 = 12
+2 X 7 = 14
+2 X 8 = 16
+2 X 9 = 18
+2 X 10 = 20
+"""
+t = int(input("what table would you like to display?"))
+for i in range(1, 11):  # Start from 1
+    print(t, "X", i, "=", t * i)
+"""
+2 X 1 = 2
+2 X 2 = 4
+2 X 3 = 6
+2 X 4 = 8
+2 X 5 = 10
+2 X 6 = 12
+2 X 7 = 14
+2 X 8 = 16
+2 X 9 = 18
+2 X 10 = 20
+"""
