b203bbebb22d8ee222c068ae173af77146d702ae
Emitta2 <145447703+Emitta2@users.noreply.github.com>
2025-06-03 16:27:00 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,21 @@
+print("Hello everyone,we are starting")
+n=1
+c=1
+while(c==1):
+    print("Token number",n,"may please come in")
+    c=int(input("Continue?(0?1)"))
+    n=n+1
+print("Thank you,this is the end of our day")
+
+"""
+Hello everyone,we are starting
+Token number 1 may please come in
+Continue?(0?1)1
+Token number 2 may please come in
+Continue?(0?1)1
+Token number 3 may please come in
+Continue?(0?1)1
+Token number 4 may please come in
+Continue?(0?1)0
+Thank you,this is the end of our day
+"""
